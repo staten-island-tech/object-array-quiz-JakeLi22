5a3bff95d11d7b89c514df03961881da6d4cdf31
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-03-01 12:20:51 -0500

    attempt 1

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const presidents = [
   { name: "Richard Nixon", terms: 2, party: "R" },
   { name: "John F. Kennedy", terms: 1, party: "D" },
   { name: "Lyndon Johnson", terms: 2, party: "D" },
-  { name: "Dwight Eisenhower", terms: 2, party: "R" }
+  { name: "Dwight Eisenhower", terms: 2, party: "R" },
 ];
 
 //Challenge Questions
 //1) Filter all presidents, leaving only the Democratic ones
+const democrats = presidents.filter((president) => (president.party = "D"));
+console.table(democrats);
 
 //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
 
